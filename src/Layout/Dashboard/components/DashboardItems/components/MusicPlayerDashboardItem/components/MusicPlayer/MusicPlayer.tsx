6bd5e2abbad9c768f1f3e1618ecb1483e1e891dc
shiels <thebelfastflygon@gmail.com>
2022-08-13 23:13:19 +0200

    play different genres of music

--- src/Layout/Dashboard/components/DashboardItems/components/MusicPlayerDashboardItem/components/MusicPlayer/MusicPlayer.tsx
+++ src/Layout/Dashboard/components/DashboardItems/components/MusicPlayerDashboardItem/components/MusicPlayer/MusicPlayer.tsx
@@ -1,15 +1,64 @@
-import song from "@assets/music/lofi.mp3"
+import lofi from "@assets/music/lofi.mp3";
+import punk from "@assets/music/punk.mp3";
+import rock from "@assets/music/rock.mp3";
 
+import { Segmented, Space } from "antd";
+import { useState } from "react";
 
-const MusicPlayer = ()=> {
-    
-    return(
-        <>
-        <audio controls>
-        <source src={song} type="audio/mpeg" /> 
-        </audio>
-        </>
-    )
-}
+const MusicPlayer = () => {
+  const [genre, setGenre] = useState("Rock");
+
+  const createRockPlayer = () => {
+    return (
+      <audio controls controlsList="nodownload">
+        <source src={rock} type="audio/mpeg" />
+      </audio>
+    );
+  };
+
+  const createLofiPlayer = () => {
+    return (
+      <audio controls controlsList="nodownload">
+        <source src={lofi} type="audio/mpeg" />
+      </audio>
+    );
+  };
+
+  const createPunkPlayer = () => {
+    return (
+      <audio controls controlsList="nodownload">
+        <source src={punk} type="audio/mpeg" />
+      </audio>
+    );
+  };
+
+  const onChange = (value: string | number) => {
+    console.log(value);
+    if (value === "Rock") {
+      setGenre("Rock");
+    } else if (value === "Punk") {
+      setGenre("Punk");
+    } else {
+      setGenre("Lofi");
+    }
+
+    return value;
+  };
+
+  return (
+    <>
+      <Space direction="vertical" size="large" style={{ width: "100%" }}>
+        <Segmented
+          block
+          onChange={onChange}
+          options={["Rock", "Lofi", "Punk"]}
+        />
+        {genre === "Rock" ? createRockPlayer() : ""}
+        {genre === "Lofi" ? createLofiPlayer() : ""}
+        {genre === "Punk" ? createPunkPlayer() : ""}
+      </Space>
+    </>
+  );
+};
 
 export default MusicPlayer;
--- src/Layout/DashboardFooter/DashboardFooter.tsx
+++ src/Layout/DashboardFooter/DashboardFooter.tsx
@@ -1,7 +1,7 @@
 import Footer from "antd/lib/layout/layout";
 import Text from "@constants/Text";
 const DashboardFooter = () => {
-  return <Footer className="footer">{Text.FooterMessage}</Footer>;
+  return <Footer className="dashboardFooter">{Text.FooterMessage}</Footer>;
 };
 
 export default DashboardFooter;
